d6c5ad2b4a2c7732b3374dc68b552e2d203cd66a
frd2100 <92338767+frd2100@users.noreply.github.com>
2024-04-23 10:17:39 -0400

    add username update

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUsers, getUserBySessionToken, deleteUserById } from '../db/users';
+import { getUsers, deleteUserById, getUserById } from '../db/users';
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
@@ -22,7 +22,29 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
 
 
     } catch (error) {
-        console.log(error),  'errore nella delete';
+        console.log(error);
+        res.sendStatus(400);
+    }
+};
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const {id} = req.params;
+        const {username} = req.body;
+
+        if(!username) {
+            return res.sendStatus(400);
+        }
+
+        const user = await getUserById(id);
+        user.username = username;
+
+        await user.save();
+
+        return res.status(200).json(user).end();
+
+    } catch (error) {
+        console.log(error);
         res.sendStatus(400);
     }
 };
