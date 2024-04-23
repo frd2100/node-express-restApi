0c6ac45ab93ce0f1cb3e54b35154d7718912ccb3
frd2100 <92338767+frd2100@users.noreply.github.com>
2024-04-23 00:36:39 -0400

    add delete

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUsers, getUserBySessionToken } from '../db/users';
+import { getUsers, getUserBySessionToken, deleteUserById } from '../db/users';
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
@@ -9,6 +9,20 @@ export const getAllUsers = async (req: express.Request, res: express.Response) =
 
     } catch (error) {
         console.log(error);
+        res.sendStatus(400);
+    }
+};
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const {id} = req.params;
+        const deleteUser = deleteUserById(id);
+
+        return res.status(200).json(deleteUser);
+
+
+    } catch (error) {
+        console.log(error),  'errore nella delete';
         res.sendStatus(400);
     }
 };
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,14 +1,27 @@
 import express from 'express';
 import {get, merge} from 'lodash'
-import { getUsers } from '../db/users';
+import { getUserBySessionToken } from '../db/users';
 
-export const getAllUsers(req: express.Request, res: express.Response, next: express.NextFunction) => {
+export const isAuthenticated = async(req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-        const users = await getUsers()
-        return  res.sendStatus(200).json(users);
+        const sessionToken = req.cookies['FEDERICO-AUTH'];
+
+        if(!sessionToken){
+            return res.sendStatus(403);
+        }
+
+        const existingUser =  await getUserBySessionToken(sessionToken);
+
+        if(!existingUser){
+            return res.sendStatus(403);
+        }
+
+        merge(req, {identity: existingUser});
+        return next();
 
     } catch (error) {
         console.log(error);
-        res.sendStatus(400);
+        return res.sendStatus(400);
+
     }
 }
