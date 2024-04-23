6c95c222e09c527e47b618cc56c8e4b5d0e8debf
frd2100 <92338767+frd2100@users.noreply.github.com>
2024-04-23 00:14:38 -0400

    add get  users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,2 +1,14 @@
 import express from 'express';
-import { getUsers } from '../db/users';
+import { getUsers, getUserBySessionToken } from '../db/users';
+
+export const getAllUsers = async (req: express.Request, res: express.Response) => {
+    try {
+        const users = await getUsers();
+
+        return res.status(200).json(users);
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(400);
+    }
+};
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,26 +1,14 @@
 import express from 'express';
 import {get, merge} from 'lodash'
-import { getUserBySessionToken } from '../db/users';
+import { getUsers } from '../db/users';
 
-export const isAuthentichatedasync(req: express.Request, res: express.Response, next: express.NextFunction) => {
+export const getAllUsers(req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-        const sessionToken = req.cookies['FEDERICO-AUTH'];
+        const users = await getUsers()
+        return  res.sendStatus(200).json(users);
 
-        if(!sessionToken){
-            return res.sendStatus(403);
-        }
-
-        const existingUser =  await getUserBySessionToken(sessionToken);
-
-        if(!existingUser){
-            return res.sendStatus(403);
-        }
-
-        merge(req, {identity: existingUser});
-        
     } catch (error) {
         console.log(error);
-        );
-        
+        res.sendStatus(400);
     }
 }
